f089283856d26d250b8b86d70d544047b21ad4cc
Quoc Hung <quochung.064@gmail.com>
2020-08-27 15:28:15 -0500

    seperate scraping script to schedule time for updating database

---
+++ show_news/management/commands/scrape.py
@@ -0,0 +1,15 @@
+from show_news.models import Articles, Source_Of_News
+from django.core.management.base import BaseCommand
+from show_news.helper import _get_content, _update_database, _get_news
+
+
+class Command(BaseCommand):
+    help = "update database"
+    # define logic of command
+
+    def handle(self, *args, **options):
+        pages = Source_Of_News.objects.all()
+        number_of_articles = 0
+        for page in pages:
+            number_of_articles += _get_news(page.page_url, page.page_name)
+        self.stdout.write('Update completed with ' + str(number_of_articles) + ' new articles')
